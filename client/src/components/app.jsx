0dda8df40741b928cfa70173bda1352dde65ecec
Justus <justuskwildenradt@gmail.com>
2018-06-02 15:14:42 -0700

    Added getReviewData function to retreive Review data and update state

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomId: 5,
+      reviewData: [],
+    }
   }
+
+  componentDidMount() {
+    this.getReviewData(this.handleReceivedReviewData.bind(this));
+  }
+
+  getReviewData(callback) {
+    axios.get('/reviews', {
+      params: {
+        ID: this.state.roomId,
+      }
+    })
+    .then((response) => {
+      console.log('success got data', response.data);
+      callback(response.data);
+    })
+    .catch(error => console.error('failed to get room data', error))
+  }
+
+  handleReceivedReviewData(data) {
+    this.setState({
+      reviewData: data,
+    })
+  }
+
   render() {
     return (
       <div>hello world</div>
