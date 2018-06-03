99e428330b89faacc1cb695d167ec7f0b5dd3151
Justus <justuskwildenradt@gmail.com>
2018-06-03 12:50:37 -0700

    Added a thank you popup when the report submit button is clicked

--- client/src/components/report.jsx
+++ client/src/components/report.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
+import ReportThankyou from './reportThankyou.jsx';
 
 const Report = () => (
   <Popup trigger={<button>report</button>} position="right center">
-    <a className="close" onClick={close}>&times</a>
-    <div>Do you want to anonymously report this review?</div>
-    <div>If so, please choose one of the following reasons.<button className="readMore">Learn more</button></div>
-    <input type="radio" name="review_flags" value="Inappropriate content" /><span>Inappropriate content</span>
-    <div>This review contains violent, graphic, promotional, or otherwise offensive content.</div>
-    <input type="radio" name="review_flags" value="Dishonest or hateful content" /><span>Dishonest or hateful content</span>
-    <div>This review is purposefully malicious and assaulting.</div>
-    <input type="radio" name="review_flags" value="Fake content" /><span>Fake content</span>
-    <div>This review contains false information or may be fake.</div>
-    <input className="submitReport" type="submit" value="submit" />
+
+    {close => (
+    <div>
+      <a className="close" onClick={close}>
+        &times;
+      </a>
+      <div>Do you want to anonymously report this review?</div>
+      <div>If so, please choose one of the following reasons.<button className="readMore">Learn more</button></div>
+      <input type="radio" name="review_flags" value="Inappropriate content" /><span>Inappropriate content</span>
+      <div>This review contains violent, graphic, promotional, or otherwise offensive content.</div>
+      <input type="radio" name="review_flags" value="Dishonest or hateful content" /><span>Dishonest or hateful content</span>
+      <div>This review is purposefully malicious and assaulting.</div>
+      <input type="radio" name="review_flags" value="Fake content" /><span>Fake content</span>
+      <div>This review contains false information or may be fake.</div>
+      <div><ReportThankyou /></div>
+    </div>
+    )}
   </Popup>
 );
 
 export default Report;
+
+
+{/* <Popup trigger={<button>Trigger</button>} position="top left">
+{close => (
+  <div>
+    Content here
+    <a className="close" onClick={close}>
+      &times;
+    </a>
+  </div>
+)}
+</Popup> */}
+
 
 // import React from "react";
 // import Warper from "./Warper";
---
+++ client/src/components/reportThankyou.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Popup from 'reactjs-popup';
+
+const ReportThankyou = () => (
+  <Popup trigger={<input className="submitReport" type="submit" value="submit" />} position="right center">
+
+    {close => (
+    <div className="reportThankyou">
+      <a className="thankyouClose" onClick={close}>
+        &times;
+      </a>
+      <div className="thankyou">Thank you</div>
+      <div className="thankyouText">These reports help make Airbnb better for everyone, so we take them seriously. We'll reach out if we have any questions about your report.</div>
+    </div>
+    )}
+  </Popup>
+);
+
+export default ReportThankyou;
