ca0fd03add332126aed9111ed8f2a27d19012a2a
Justus <justuskwildenradt@gmail.com>
2018-06-05 08:28:27 -0700

    Pages component conditionally renders next and prev buttons

--- client/src/components/pages.jsx
+++ client/src/components/pages.jsx
@@ -2,21 +2,43 @@ import React from 'react';
 import PropType from 'prop-types';
 import Page from './page';
 
-const Pages = props => (
-  <div>
-    <button onClick={props.handlePrevClick}>Prev</button>
+const Pages = (props) => {
+  if (props.currentPage === 0) {
+    return (
+      <div>
+        <div>
+          {props.numberOfPages.map(page => <Page page={page} key={page} handlePageClick={props.handlePageClick} />)}
+        </div>
+        <button onClick={props.handleNextClick}>Next</button>
+      </div>
+    );
+  } else if (props.currentPage === props.numberOfPages[props.numberOfPages.length - 2]) {
+    return (
+      <div>
+        <button onClick={props.handlePrevClick}>Prev</button>
+        <div>
+          {props.numberOfPages.map(page => <Page page={page} key={page} handlePageClick={props.handlePageClick} />)}
+        </div>
+      </div>
+    );
+  }
+  return (
     <div>
-      {props.numberOfPages.map(page => <Page page={page} key={page} handlePageClick={props.handlePageClick} />)}
+      <button onClick={props.handlePrevClick}>Prev</button>
+      <div>
+        {props.numberOfPages.map(page => <Page page={page} key={page} handlePageClick={props.handlePageClick} />)}
+      </div>
+      <button onClick={props.handleNextClick}>Next</button>
     </div>
-    <button onClick={props.handleNextClick}>Next</button>
-  </div>
-);
+  );
+};
 
 Pages.propTypes = {
   handleNextClick: PropType.func.isRequired,
   handlePrevClick: PropType.func.isRequired,
   handlePageClick: PropType.func.isRequired,
   numberOfPages: PropType.arrayOf(PropType.number).isRequired,
+  currentPage: PropType.number.isRequired,
 };
 
 export default Pages;
