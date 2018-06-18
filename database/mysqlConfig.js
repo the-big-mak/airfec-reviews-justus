f8b3df769a77570fd676d1495e912794b0d52ac4
Justus <justuskwildenradt@gmail.com>
2018-06-17 20:33:55 -0700

    Added mysqlConfig to .gitignore

--- database/mysqlConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  host: 'airfec-reviews-mysql.c9xtzxski01n.us-east-1.rds.amazonaws.com',
-  user: 'justuskw',
-  password: 'Lilojet1.',
-  database: 'airFeC_reviews',
-  port: '3306',
-};
