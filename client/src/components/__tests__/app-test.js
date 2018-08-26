4d24e382fee3ce678917080c431ceeaf4a5d7ec1
Justus <justuskwildenradt@gmail.com>
2018-08-26 13:31:17 -0700

    Added test for getAverageRating

--- client/src/components/__tests__/app-test.js
+++ client/src/components/__tests__/app-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../app';
 const request = require('supertest')('http://localhost:3001');
 
@@ -27,10 +27,35 @@ describe('App Component', () => {
   it('should fetch reviews by room id', (done) => {
     request.get('/reviews/30')
       .expect((res) => {
-        console.log(res.body);
         expect(res.body.length).toBe(67);
       })
       .expect(200)
       .end(done);
   });
+  // it('should update the state with reviews when App mounts', () => {
+  //   // const shallowApp = shallow(<App />);
+  //   // const reviewState = shallowApp.state().allReviewData;
+  //   // expect(reviewState.length).toEqual(0);
+  //   // shallowApp.setState({ roomId: 30 });
+  //   // console.log('//////////', shallowApp.state().roomId);
+  //   const wrapper = mount(<App />, { lifecycleExperimental: true });
+  //   // wrapper.setState({ roomId: 30 });
+  //   expect(wrapper.instance().componentDidMount()).toHaveBeenCalledTimes(1);
+  //   // expect(reviewState.length).toEqual(67);
+
+  // });
+  it('should get the average rating', () => {
+    const data = [
+      {
+        accuracy_rating: 2,
+        communication_rating: 1,
+      },
+      {
+        accuracy_rating: 4,
+        communication_rating: 3,
+      },
+    ];
+    expect(App.getAverageRating(data, 'accuracy_rating')).toEqual(3);
+  });
+  
 });
