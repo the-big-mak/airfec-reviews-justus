df4b567875ac517745aa09a6d0b289d895f8eec3
Justus <justuskwildenradt@gmail.com>
2018-08-26 11:43:08 -0700

    Added tests and async test

--- client/src/components/__tests__/app-test.js
+++ client/src/components/__tests__/app-test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../app';
+const request = require('supertest')('http://localhost:3001');
 
 describe('App Component', () => {
   it('should render without throwing an error', () => {
@@ -15,9 +16,21 @@ describe('App Component', () => {
   it('renders a reviewList input', () => {
     expect(shallow(<App />).find('#reviewList').length).toEqual(1);
   });
+  it('renders a pages input', () => {
+    expect(shallow(<App />).find('#pages').length).toEqual(1);
+  });
   it('starts with allReviewData being an empty array', () => {
     const wrapper = shallow(<App />);
     const AppState = wrapper.state().allReviewData;
     expect(AppState.length).toEqual(0);
+  });
+  it('should fetch reviews by room id', (done) => {
+    request.get('/reviews/30')
+      .expect((res) => {
+        console.log(res.body);
+        expect(res.body.length).toBe(67);
+      })
+      .expect(200)
+      .end(done);
   });
 });
